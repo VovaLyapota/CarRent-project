49655a0971c8708d5f2e6c5284afaad1b89eaf56
VovaMyb <rimidalvatopayl@gmail.com>
2023-12-06 21:09:32 +0200

    template start options fixes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/LearnLingo-project">
+    <BrowserRouter basename="/CarRent-project">
       <App />
     </BrowserRouter>
   </React.StrictMode>
